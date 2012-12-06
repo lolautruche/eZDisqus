9e49694e1a320362d569738979df9c88356babe0
Jérôme Vieilledent <lolautruche@gmail.com>
2012-12-06 13:59:52 +0100

    Initial bundle structure

---
+++ BDunogier/EzDisqusBundle/EzDisqusBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BDunogier\EzDisqusBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EzDisqusBundle extends Bundle
+{
+}
---
+++ BDunogier/EzDisqusBundle/FieldType/DisqusComments/Value.php
@@ -0,0 +1,15 @@
+<?php
+use eZ\Publish\Core\FieldType\Value as BaseValue;
+
+class Value extends BaseValue
+{
+    /**
+     * Returns a string representation of the field value.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        // TODO: Implement __toString() method.
+    }
+}
